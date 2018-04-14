6b90b586898bc9c4fb0d19bb45aef8be10fe376c
Chris Cox <christopher.cox-2@mancheste.ac.uk>
2018-04-14 20:55:37 +0100

    demo components are basically laid out as I want them.

--- DEMO1_RSA_ROI_simulatedAndRealData.m
+++ DEMO1_RSA_ROI_simulatedAndRealData.m
@@ -3,131 +3,94 @@
 % This function computes the results in the main Figures of 
 % Nili et al. (PLoS Comp Biol 2013)
 
-clear;clc; close all hidden;
-returnHere = pwd;
-cd ..;addpath(genpath(pwd));cd(returnHere)
+addpath('rsatoolbox/Engines');
+addpath('rsatoolbox/Modules');
+DemoDir = fullfile('rsatoolbox','Demos');
 userOptions = defineUserOptions;
-mkdir('DEMO1');
-userOptions.rootPath = [pwd,filesep,'DEMO1'];
+outputDir = 'DEMO1';
+if ~(exist(outputDir,'dir') == 7)
+    mkdir(outputDir);
+end
+userOptions.rootPath = fullfile(pwd,'DEMO1');
 userOptions.analysisName = 'DEMO1';
 
-addpath('rsatoolbox/Engines');
-DemoDir = fullfile('rsatoolbox','Demos');
-%% control variables
-nSubjects=12;
-subjectPatternNoiseStd=1;
-
-nModelGrades=3;
-bestModelPatternDeviationStd=0;
-worstModelPatternDeviationStd=6;
-
-patternDistanceMeasure='correlation';
-
+nitems = 92;
 
 %% load RDMs and category definitions from Kriegeskorte et al. (Neuron 2008)
-summaryRDMs_filename = fullfile(DemoDir,'92imageData','Kriegeskorte_Neuron2008_supplementalData.mat');
+brain.filename = fullfile(DemoDir,'92imageData','Kriegeskorte_Neuron2008_supplementalData.mat');
 VariablesToLoad = {
     'categoryLabels'
     'RDMs_mIT_hIT_fig1'
     'categoryVectors'
     'stimuli_92objs'
 };
-tmp = load(summaryRDMs_filename, VariablesToLoad{:});
-summaryRDMs.RDMs = tmp.RDMs_mIT_hIT_fig1;
+tmp = load(brain.filename, VariablesToLoad{:});
+brain.RDMs = tmp.RDMs_mIT_hIT_fig1;
 Metadata = rmfield(tmp,'RDMs_mIT_hIT_fig1');
-showRDMs(summaryRDMs.RDMs_mIT_hIT_fig1,1);
-
-rdm_mIT=squareRDMs(summaryRDMs.RDMs_mIT_hIT_fig1(1).RDM);
-rdm_hIT=squareRDMs(summaryRDMs.RDMs_mIT_hIT_fig1(2).RDM);
-
-subjectRDMs_filename = fullfile(DemoDir,'92imageData','92_brainRDMs.mat');
-VariablesToLoad = {
-    'RDMs'
-};
-subjectRDMs = load(subjectRDMs_filename,VariablesToLoad{:});
-RDMs_hIT_bySubject = averageRDMs_subjectSession(subjectRDMs.RDMs, 'session');
-showRDMs(RDMs_hIT_bySubject,2);
+showRDMs(brain.RDMs,1);
 
 
 %% define categorical model RDMs
-[binRDM_animacy, nCatCrossingsRDM] = categoricalRDM(Metadata.categoryVectors(:,1),3,true);
+[binRDM_animacy, ~] = categoricalRDM(Metadata.categoryVectors(:,1),3,true);
 ITemphasizedCategories=[1 2 5 6]; % animate, inanimate, face, body
 [binRDM_cats, nCatCrossingsRDM]=categoricalRDM(Metadata.categoryVectors(:,ITemphasizedCategories),4,true);
-faceAnimateInaniClustersRDM_filename = fullfile(DemoDir,'92imageData','faceAnimateInaniClustersRDM.mat');
-VariablesToLoad = {
-    'categoryLabels'
-    'RDMs_mIT_hIT_fig1'
-    'categoryVectors'
-    'stimuli_92objs'
-    'rdm_hIT'
-    'binRDM_animacy'
-    'nCatCrossingsRDM'
-    'withinFaces'
-    'faceAnimateInaniClustersRDM'
-};
-faceAnimateInaniClustersRDM = load(faceAnimateInaniClustersRDM_filename, VariablesToLoad{:});
+handmade.filename = '';
+handmade.RDMs = [
+    struct('RDM',binRDM_animacy,'color',[0,0,0],'name','animacy')
+    struct('RDM',binRDM_cats,'color',[0,0,0],'name','facebody_inanimate')
+];
 
 %% load behavioural RDM from Mur et al. (Frontiers Perc Sci 2013)
-behavRDMs_filename = fullfile(DemoDir,'92imageData','92_behavRDMs.mat');
+behaviour.filename = fullfile(DemoDir,'92imageData','92_behavRDMs.mat');
 VariablesToLoad = {
     'rdms_behav_92'
 };
-tmp = load(behavRDMs_filename, VariablesToLoad{:});
-behavRDMs = tmp.rdms_behav_92;
-disp(struct2table(behavRDMs));
-showRDMs(behavRDMs)
+tmp = load(behaviour.filename, VariablesToLoad{:});
+disp(struct2table(tmp.rdms_behav_92));
+showRDMs(tmp.rdms_behav_92)
+% Average over subjects. Note that averageRDMs_subjectSession() infers how
+% to group by subjects based on the ordering of RDMs in the array. The
+% array must have at most 3 dimensions, and the dimensions are interpretted
+% as [RDMs, Subjects, Sessions]. So, check out our structure:
+disp(size(tmp.rdms_behav_92));
 
-rdm_simJudg = mean(stripNsquareRDMs(rdms_behav_92),3);
-
-
-%% load RDMs for V1 model and HMAX model with natural image patches from Serre et al. (Computer Vision and Pattern Recognition 2005)
-rdm92_V1model_filename = fullfile(DemoDir,'92imageData','rdm92_V1model.mat');
-VariablesToLoad = {
-    'RDMs'
-    'V1modelSimMat'
-    'modelSimMat_HMAXwithNatImPatchExt'
-    'validConditionsLOG'
-    'rdm92_V1model'
-    'rdm92_HMAXnatImPatch'
-};
-rdm92_V1model = load(rdm92_V1model_filename, VariablesToLoad{:});
-
-rdm92_HMAXnatImPatch_filename = fullfile(DemoDir,'92imageData','rdm92_HMAXnatImPatch.mat');
-VariablesToLoad = {
-    'RDMs'
-    'V1modelSimMat'
-    'modelSimMat_HMAXwithNatImPatchExt'
-    'validConditionsLOG'
-    'rdm92_V1model'
-    'rdm92_HMAXnatImPatch'
-};
-rdm92_HMAXnatImPatch = load(rdm92_HMAXnatImPatch_filename, VariablesToLoad{:});
+% This will be interpretted as containing 1 RDM for each of 16 subjects,
+% which only have 1 session recorded. That is the appropriate
+% interpretation, so we can go ahead and run averageRDMs_subjectSession():
+behaviour.RDMs = averageRDMs_subjectSession(tmp.rdms_behav_92, 'subject');
 
 %% load RADON and silhouette models and human early visual RDM
-modelRDMs_filename = fullfile(DemoDir,'92imageData','92_modelRDMs.mat');
+models.filename = cell(2,1);
+models.filename{1} = fullfile(DemoDir,'92imageData','92_modelRDMs.mat');
 VariablesToLoad = {
     'Models'
 };
-tmp = load(modelRDMs_filename);
-FourCatsRDM = tmp.Models(2).RDM;
-humanEarlyVisualRDM = tmp.Models(4).RDM;
-silhouetteRDM = tmp.Models(7).RDM;
-radonRDM = tmp.Models(8).RDM;
+tmp = load(models.filename{1}, VariablesToLoad{:});
+models.RDMs = reshape(tmp.Models([2,4,7,8]),4,1);
 
+
+%% load RDMs for V1 model and HMAX model with natural image patches from Serre et al. (Computer Vision and Pattern Recognition 2005)
+models.filename{2} = fullfile(DemoDir,'92imageData','rdm92_V1model.mat');
+VariablesToLoad = {
+    'RDMs'
+    'V1modelSimMat'
+    'modelSimMat_HMAXwithNatImPatchExt'
+    'validConditionsLOG'
+    'rdm92_V1model'
+    'rdm92_HMAXnatImPatch'
+};
+tmp = load(models.filename{2}, VariablesToLoad{:});
+models.RDMs = [
+    models.RDMs
+    struct('RDM',tmp.rdm92_V1model,'color',[0,0,0],'name','V1model')
+    struct('RDM',tmp.rdm92_HMAXnatImPatch,'color',[0,0,0],'name','HMAXnatImPatch')
+];
+showRDMs(models.RDMs',5);
 
 %% concatenate and name the modelRDMs
-modelRDMs=cat(3,binRDM_animacy,faceAnimateInaniClustersRDM,FourCatsRDM,rdm_simJudg,humanEarlyVisualRDM,rdm_mIT,silhouetteRDM,rdm92_V1model,rdm92_HMAXnatImPatch,radonRDM,gradedModelRDMs);
-modelRDMs=wrapAndNameRDMs(modelRDMs,{'ani./inani.','face/ani./inani.','face/body/nat./artif.','sim. judg.','human early visual','monkey IT','silhouette','V1 model','HMAX-2005 model','RADON','true model','true with noise','true with more noise'});
-modelRDMs=modelRDMs(1:end-2); % leave out the true with noise models
-
-showRDMs(modelRDMs,5);
-handleCurrentFigure([userOptions.rootPath,filesep,'allModels'],userOptions);
 % place the model RDMs in cells in order to pass them to
 % compareRefRDM2candRDMs as candidate RDMs
-modelRDMs_cell = cell(numel(modelRDMs),1);
-for modelRDMI=1:numel(modelRDMs)
-    modelRDMs_cell{modelRDMI}=modelRDMs(modelRDMI);
-end
+modelRDMs_cell = num2cell(models.RDMs);
 
 %% activity pattern MDS
 categoryIs=[5 6 7 8];
@@ -139,54 +102,54 @@ categoryCols=[0 0 0
               1 0 0
               0 1 0
               0 0.5 1];
+          
+blankConditionLabels = cell(1, nitems);
+[blankConditionLabels{:}] = deal(' ');
 
-
-
-% MDS plot
-categoryIs=[5 6 7 8];
-categoryCols=[0 0 0
-    0 0 0
-    0 0 0
-    0 0 0
-    1 0.5 0
-    1 0 0
-    0 1 0
-    0 0.5 1];
-
-
+userOptions.conditionColours = [];
+userOptions.distanceMeasure = 'Spearman';
+userOptions.RDMcorrelationType = 'Kendall_taua';
+userOptions.conditionLabels = blankConditionLabels;
 for condI = 1:92
     for catI = 1:numel(categoryIs)
-        if categoryVectors(condI,categoryIs(catI))
+        if Metadata.categoryVectors(condI,categoryIs(catI))
             userOptions.conditionColours(condI,:) = categoryCols(categoryIs(catI),:);
         end
     end
 end
-avgRDM.RDM = avgSubjectRDM;
+avgRDM = selectbyfield(brain.RDMs,'name','hITvisStim_316vx');
 avgRDM.name = 'subject-averaged RDM';
 avgRDM.color = [0 0 0];
-[blankConditionLabels{1:size(modelRDMs_cell{1}.RDM,1)}] = deal(' ');
 
-% true-model MDS
-rsa.MDSConditions(modelRDMs_cell{11}, userOptions,struct('titleString','ground-truth MDS',...
-    'fileName','trueRDM_MDS','figureNumber',6));
-% true-model dendrogram
 
-rsa.dendrogramConditions(modelRDMs_cell{11}, userOptions,...
-struct('titleString', 'Dendrogram of the ground truth RDM', 'useAlternativeConditionLabels', true, 'alternativeConditionLabels', {blankConditionLabels}, 'figureNumber', 7));
-% subject-averaged MDS
-rsa.MDSConditions(avgRDM, userOptions,struct('titleString','subject-averaged MDS',...
-    'fileName','ssMDS','figureNumber',8));
+%% subject-averaged MDS
+
+MDSConditions(avgRDM, userOptions,struct('titleString','subject-averaged MDS',...
+    'fileName','ssMDS', 'figureNumber',8));
 % subject-averaged Dendrogram
-rsa.dendrogramConditions(avgRDM, userOptions,...
-struct('titleString', 'Dendrogram of the subject-averaged RDM', 'useAlternativeConditionLabels', true, 'alternativeConditionLabels', {blankConditionLabels}, 'figureNumber', 9));
+dendrogramConditions(avgRDM, userOptions, struct( ...
+    'titleString', 'Dendrogram of the subject-averaged RDM', ...
+    'useAlternativeConditionLabels', true, ...
+    'alternativeConditionLabels', ...
+    {blankConditionLabels}, ...
+    'figureNumber', 9));
 
-% one-subject MDS (e.g. simulated subject1), noisier
-rsa.MDSConditions(rsa.rdm.wrapAndNameRDMs(subjectRDMs(:,:,1),{'single-subject RDM'}), userOptions,struct('titleString','sample subject MDS',...
+%% one-subject MDS
+hIT_bySubjectAndSession.filename = fullfile(DemoDir,'92imageData','92_brainRDMs.mat');
+VariablesToLoad = {
+    'RDMs'
+};
+hIT_bySubjectAndSession = load(hIT_bySubjectAndSession.filename,VariablesToLoad{:});
+hIT_bySubject.RDMs = averageRDMs_subjectSession(hIT_bySubjectAndSession.RDMs, 'session');
+
+singleSubjectRDM = hIT_bySubject.RDMs(1);
+singleSubjectRDM.name = 'hIT-BE';
+MDSConditions(singleSubjectRDM, userOptions,struct('titleString','sample subject MDS',...
     'fileName','single-subject RDM','figureNumber',10));
-
-% one-subject Dendrogram
-rsa.dendrogramConditions(rsa.rdm.wrapAndNameRDMs(subjectRDMs(:,:,3),{'single-subject RDM'}), userOptions,...
-struct('titleString', 'Dendrogram of a single-subject RDM', 'useAlternativeConditionLabels', true, 'alternativeConditionLabels', {blankConditionLabels}, 'figureNumber', 11));
+% 
+% % one-subject Dendrogram
+% rsa.dendrogramConditions(rsa.rdm.wrapAndNameRDMs(subjectRDMs(:,:,3),{'single-subject RDM'}), userOptions,...
+% struct('titleString', 'Dendrogram of a single-subject RDM', 'useAlternativeConditionLabels', true, 'alternativeConditionLabels', {blankConditionLabels}, 'figureNumber', 11));
 
 
 
@@ -194,31 +157,18 @@ struct('titleString', 'Dendrogram of a single-subject RDM', 'useAlternativeCondi
 % 2nd order correlation matrix
 userOptions.RDMcorrelationType='Kendall_taua';
 
-rsa.pairwiseCorrelateRDMs({avgRDM, modelRDMs}, userOptions, struct('figureNumber', 12,'fileName','RDMcorrelationMatrix'));
+pairwiseCorrelateRDMs({avgRDM, models.RDMs}, userOptions, struct( ...
+    'figureNumber', 12, ...
+    'fileName','RDMcorrelationMatrix'));
 
 % 2nd order MDS
-rsa.MDSRDMs({avgRDM, modelRDMs}, userOptions, struct('titleString', 'MDS of different RDMs', 'figureNumber', 13,'fileName','2ndOrderMDSplot'));
+MDSRDMs({avgRDM, models.RDMs}, userOptions, struct( ...
+    'titleString', 'MDS of different RDMs', ...
+    'figureNumber', 13, ...
+    'fileName','2ndOrderMDSplot'));
 
 
-%% statistical inference
-userOptions.RDMcorrelationType='Kendall_taua';
-userOptions.RDMrelatednessTest = 'subjectRFXsignedRank';
-userOptions.RDMrelatednessThreshold = 0.05;
-userOptions.RDMrelatednessMultipleTesting = 'FDR';
-userOptions.saveFiguresPDF = 1;
-userOptions.candRDMdifferencesTest = 'subjectRFXsignedRank';
-userOptions.candRDMdifferencesThreshold = 0.05;
-userOptions.candRDMdifferencesMultipleTesting = 'FDR';
-userOptions.plotpValues = '=';
-userOptions.barsOrderedByRDMCorr=true;
-userOptions.resultsPath = userOptions.rootPath;
-userOptions.figureIndex = [14 15];
-userOptions.figure1filename = 'compareRefRDM2candRDMs_barGraph_simulatedITasRef';
-userOptions.figure2filename = 'compareRefRDM2candRDMs_pValues_simulatedITasRef';
-stats_p_r=rsa.compareRefRDM2candRDMs(subjectRDMs, modelRDMs_cell, userOptions);
-
-
-%% Finally: real fMRI data (human IT RDM from Kriegeskorte et al. (Neuron 2008) as the reference RDM
+%% Finally: the anaysis! (human IT RDM from Kriegeskorte et al. (Neuron 2008) as the reference RDM
 % userOptions.RDMcorrelationType='Kendall_taua';
 userOptions.RDMcorrelationType='Spearman';
 
@@ -235,4 +185,4 @@ userOptions.candRDMdifferencesMultipleTesting = 'FDR';
 userOptions.figure1filename = 'compareRefRDM2candRDMs_barGraph_hITasRef';
 userOptions.figure2filename = 'compareRefRDM2candRDMs_pValues_hITasRef';
 userOptions.figureIndex = [16 17];
-stats_p_r=rsa.compareRefRDM2candRDMs(RDMs_hIT_bySubject, modelRDMs_cell(1:end-1), userOptions);
+stats_p_r=compareRefRDM2candRDMs(hIT_bySubject.RDMs, modelRDMs_cell, userOptions);
--- stackLowerTriangles.m
+++ stackLowerTriangles.m
@@ -5,18 +5,15 @@ function [ StackedMatrix ] = stackLowerTriangles( RDMs )
         m = numel(RDMs);
         n = size(RDMs(1).RDM,1);
         StackedMatrix = preallocateStackedMatrix(m,n);
-        z = defineLowerTriangleMask(n);
         for i = 1:m
-            StackedMatrix(i,:) = RDMs(i).RDM(z);
+            StackedMatrix(i,:) = squareform(RDMs(i).RDM,'tovector');
         end
     elseif isnumeric(RDMs)
         m = size(RDMs,3);
         n = size(RDMs,1);
         StackedMatrix = preallocateStackedMatrix(m,n);
-        z = defineLowerTriangleMask(n);
         for i = 1:m
-            tmp = RDMs(:,:,i);
-            StackedMatrix(i,:) = tmp(z);
+            StackedMatrix(i,:) = squareform(RDMs(:,:,i),'tovector');
         end
     end
 end
@@ -24,8 +21,4 @@ end
 function EmptyStackedMatrix = preallocateStackedMatrix(m,n)
     N = ((n.^2) - n) ./ 2;
     EmptyStackedMatrix = zeros(m,N);
-end
-
-function LowerTriangleMask = defineLowerTriangleMask(n)
-    LowerTriangleMask = tril(true(n),-1);
 end
