0712dc51923bbffcf358db9068c9aca77b2c7fb1
Chris Cox cox.crc@gmail.com <christopher.cox-2@manchester.ac.uk>
2018-04-16 18:58:24 +0100

    Revised and refactored the Searchlight Demo (DEMO4)

--- DEMO4_RSAsearchlight_sim.m
+++ DEMO4_RSAsearchlight_sim.m
@@ -3,53 +3,75 @@
 % analysis using RSA, computes the similarity maps for each subject
 % and does group level inference.
 
-% demo of the searchlight analysis
-
 %% SETUP
 % Add RSA Toolbox components to the path. This will allow matlab to find
 % the functions defined within the toolbox when we ask to use them.
-addpath(fullfile('rsatoolbox','Engines'));
-addpath(fullfile('rsatoolbox','Modules'));
+rootDir = pwd();
+addpath(fullfile(rootDir,'rsatoolbox','Engines'));
+addpath(fullfile(rootDir,'rsatoolbox','Modules'));
+addpath(fullfile(rootDir,'HelperFunctions'));
+addpath(fullfile(rootDir,'Patches'));
 
 % Some of the important Network RSA functions reference a unified set of
 % "User Options". This helper-function will get us started.
 userOptions = BarebonesRSASetup('DEMO4');
-userOptions.betaPath = fullfile(pwd(),'demoTest','[[subjectName]]','[[betaIdentifier]]');
-% Which distance measure to use when calculating first-order RDMs.
-userOptions.distance = 'Correlation';
+userOptions.voxelSize = [3 3 3];
 
-% Let's define the path to the Demo Data distributed with the RSA Toolbox.
-DemoDataDir = fullfile('rsatoolbox','Demos','92imageData');
-rootDir = pwd;
+% userOptions.betaPath = fullfile(rootDir,'demoTest','[[subjectName]]','[[betaIdentifier]]');
+ReadChrisNotesAbout('userOptions.betaPath');
 
-% Generate a simulationOptions structure.
+% userOptions.distance = 'correlation';
+ReadChrisNotesAbout('userOptions.distance');
+
+% Define the path to the Demo Data distributed with the RSA Toolbox.
+DemoDataDir = fullfile(rootDir,'rsatoolbox','Demos','92imageData');
+DemoSupplementalDataDir = fullfile(rootDir,'rsatoolbox','Demos');
+
+
+% Generate a simulationOptions structure
+% --------------------------------------
+% I pre-ran all the data simulation and searchlights to save time. We'll
+% jump in at the point of having "information maps" that can be submitted
+% to statistical analysis.
 simulationOptions = simulationOptions_demo_SL();
 
+% Generate a searchlightOptions structure
+% ---------------------------------------
+% I pre-ran all the data simulation and searchlights to save time. We'll
+% jump in at the point of having "information maps" that can be submitted
+% to statistical analysis.
+userOptions.searchlightRadius = 9;
 searchlightOptions.monitor = false;
 searchlightOptions.fisher = true;
 searchlightOptions.nSessions = 1;
 searchlightOptions.nConditions = 40;
-load([rootDir,filesep,'sampleMask_org.mat'])
-load([rootDir,filesep,'anatomy.mat']);% load the resliced structural image
-% models = constructModelRDMs(modelRDMs_SL_sim, userOptions);
-load(fullfile('DEMO4','RDMs','DEMO4_Models.mat'),'Models');
-load(fullfile('DEMO4','exampleSimulatedSubject_DesignMatrix.mat'), 'fMRI_sub');
-nCond = searchlightOptions.nConditions;
+tmp = load(fullfile(DemoSupplementalDataDir,'sampleMask_org.mat'), 'm');
+SLMetadata.mask = tmp.m;
+tmp = load(fullfile(DemoSupplementalDataDir,'anatomy.mat'), 'anatVol');
+SLMetadata.anatVol = tmp.anatVol;
 Nsubjects = 20;
 
+% Generate a warpFlags structure
+% ---------------------------------------
+% I pre-ran all the data simulation and searchlights to save time. We'll
+% jump in at the point of having "information maps" that can be submitted
+% to statistical analysis.
 warpFlags.interp = 1;
 warpFlags.wrap = [0 0 0];
-userOptions.voxelSize = [3 3 3];
 warpFlags.vox = userOptions.voxelSize; % [3 3 3.75]
 warpFlags.bb = [-78 -112 -50; 78 76 85];
 warpFlags.preserve = 0;
-mapsFilename = [userOptions.analysisName, '_fMRISearchlight_Maps.mat'];
-RDMsFilename = [userOptions.analysisName, '_fMRISearchlight_RDMs.mat'];
-DetailsFilename = [userOptions.analysisName, '_fMRISearchlight_Details.mat'];
+
+% Set paths for output files that help document the analysis
+% ----------------------------------------------------------
+mapsFilename = strjoin({userOptions.analysisName, 'fMRISearchlight_Maps.mat'}, '_');
+RDMsFilename = strjoin({userOptions.analysisName, 'fMRISearchlight_RDMs.mat'}, '_');
+DetailsFilename = strjoin({userOptions.analysisName, 'fMRISearchlight_Details.mat'}, '_');
 
 %% Second-order analysis
 % Which similarity-measure is used for the second-order comparison.
 userOptions.distanceMeasure = 'Spearman';
+ReadChrisNotesAbout('userOptions.distanceMeasure');
 
 % How many permutations should be used to test the significance of the fits?  (10,000 highly recommended.)
 userOptions.significanceTestPermutations = 1000; % should be 10000
@@ -58,15 +80,15 @@ userOptions.significanceTestPermutations = 1000; % should be 10000
 userOptions.rankTransform = true;
 
 % RDM Colourscheme
-userOptions.colourScheme = jet(64);
+% userOptions.colourScheme = jet(64);
+% 
+% % Should rubber bands be shown on the MDS plot?
+% userOptions.rubberbands = true;
+% 
+% % What criterion shoud be minimised in MDS display?
+% userOptions.criterion = 'metricstress';
 
-% Should rubber bands be shown on the MDS plot?
-userOptions.rubberbands = true;
-
-% What criterion shoud be minimised in MDS display?
-userOptions.criterion = 'metricstress';
-
-% How should figures be outputted?
+% How should figures be output?
 userOptions.displayFigures = true;
 userOptions.saveFiguresPDF = true;
 userOptions.saveFiguresFig = false;
@@ -82,105 +104,40 @@ userOptions.RDMname = 'referenceRDM';
 userOptions.plottingStyle = 2;
 
 %% simulate the data and compute the correlation maps per subject
+% % Takes about 15 minutes to run ...
+% t = tic();
+% Models = constructModelRDMs(modelRDMs_SL_sim(), userOptions);
+% SimulateFMRIDataAndGenerateInformationMaps( ...
+%     Nsubjects, ...
+%     Models, ...
+%     SLMetadata.mask, ...
+%     simulationOptions, ...
+%     searchlightOptions, ...
+%     userOptions);
 % 
-% for subI = 1:Nsubjects
-%     subject = ['subject',num2str(subI)];
-%     maskName = 'mask';
-%     fprintf(['simulating fullBrain volumes for subject %d \n'],subI)
-%     [B_true,Mask,Y_true, fMRI_sub] = simulateClusteredfMRIData_fullBrain(simulationOptions);
-%     B_noisy = fMRI_sub.B;
-%     singleSubjectVols = B_noisy';
-%     userOptions.searchlightRadius = 9;mask = m;
-%     fprintf(['computing correlation maps for subject %d \n'],subI)
-%     [rs, ps, ns, searchlightRDMs.(subject)] = searchlightMapping_fMRI(singleSubjectVols, models, mask, userOptions, searchlightOptions);
-%     gotoDir(userOptions.rootPath, 'Maps');
-%     save(['rs_',subject,'.mat'],'rs');
-%     clear rs searchlightRDMs
-%     cd(rootDir)
-% end
+% TimeToSimulateAndMap = toc(t);
+
 %% display the design matrix, model RDMs and simulated RDMs and the SL
-selectPlot(1);
-subplot(321);imagesc(fMRI_sub.X);axis square
-ylabel('\bfscans');xlabel('\bfregressors')
-title('\bfdesign matrix')
+load(fullfile(userOptions.rootPath,'RDMs','DEMO4_Models.mat'),'Models');
+load(fullfile(userOptions.rootPath,'Maps','SimulationMetadata.mat'), 'fMRI_sub');
 
-subplot(322);plot(fMRI_sub.X(:,12:14))
-xlabel('scans');title('\bfregressors for 3 example conditions')
-
-subplot(323);
-image(scale01(rankTransform_equalsStayEqual(squareform(pdist(fMRI_sub.groundTruth)),1)),'CDataMapping','scaled','AlphaData',~isnan(squareform(pdist(fMRI_sub.groundTruth))));
-axis square off
-title('\bfsimulated ground truth RDM')
-
-subplot(324);
-image(scale01(rankTransform_equalsStayEqual(Models(1).RDM,1)),'CDataMapping','scaled','AlphaData',~isnan(Models(1).RDM));
-axis square off
-colormap(RDMcolormap)
-title('\bftested model RDM')
-
-
-relRoi = sphericalRelativeRoi(userOptions.searchlightRadius,userOptions.voxelSize);
-nVox_searchlight = size(relRoi,1);
-showVoxObj(relRoi+repmat(simulationOptions.effectCen,[nVox_searchlight,1]),1,[3 2 5],1,struct('x','x','y','y','z','z'),[0   0.5   0.25  1 0.4])
-title(['\bf searchlight with ',num2str(nVox_searchlight),' voxels'])
-handleCurrentFigure([rootDir,filesep,'DEMO4',filesep,'SLsimulationSettings'],userOptions);
+PlotSimulationDesign( fMRI_sub.X, fMRI_sub.groundTruth, Models(1), userOptions, simulationOptions );
 
 %% load the previously computed rMaps and concatenate across subjects
 % prepare the rMaps:
+rMaps = cell(Nsubjects,1);
 for subjectI = 1:Nsubjects
-    load([userOptions.rootPath,filesep,'Maps',filesep,'rs_subject',num2str(subjectI),'.mat'])
+    load(fullfile(userOptions.rootPath,'Maps',sprintf('rs_subject%d.mat',subjectI)))
     rMaps{subjectI} = rs;
-    fprintf(['loading the correlation maps for subject %d \n'],subjectI);
+    fprintf('loading the correlation maps for subject %d\n',subjectI);
 end
-% concatenate across subjects
-for modelI = 1:numel(Models)
-    for subI = 1:Nsubjects
-        thisRs = rMaps{subI};
-        thisModelSims(:,:,:,subI) = thisRs(:,:,:,modelI);
-    end
-    % obtain a pMaps from applying a 1-sided signrank test and also t-test to
-    % the model similarities:
-    for x=1:size(thisModelSims,1)
-        for y=1:size(thisModelSims,2)
-            for z=1:size(thisModelSims,3)
-                if mask(x,y,z) == 1
-                    [h p1(x,y,z)] = ttest(squeeze(thisModelSims(x,y,z,:)),0,0.05,'right');
-                    [p2(x,y,z)] = signrank_onesided(squeeze(thisModelSims(x,y,z,:)));
-                else
-                    p1(x,y,z) = NaN;
-                    p2(x,y,z) = NaN;
-                end
-            end
-        end
-        disp(x)
-    end
-    % apply FDR correction
-    pThrsh_t = FDRthreshold(p1,0.05,mask);
-    pThrsh_sr = FDRthreshold(p2,0.05,mask);
-    p_bnf = 0.05/sum(mask(:));
-    % mark the suprathreshold voxels in yellow
-    supraThreshMarked_t = zeros(size(p1));
-    supraThreshMarked_t(p1 <= pThrsh_t) = 1;
-    supraThreshMarked_sr = zeros(size(p2));
-    supraThreshMarked_sr(p2 <= pThrsh_sr) = 1;
-    
-    % display the location where the effect was inserted (in green):
-    brainVol = addRoiToVol(map2vol(anatVol),mask2roi(mask),[1 0 0],2);
-    brainVol_effectLoc = addBinaryMapToVol(brainVol,Mask.*mask,[0 1 0]);
-    showVol(brainVol_effectLoc,'simulated effect [green]',2);
-    handleCurrentFigure([rootDir,filesep,'DEMO4',filesep,'results_DEMO4_simulatedEffectRegion'],userOptions);
-    
-    % display the FDR-thresholded maps on a sample anatomy (signed rank test) :
-    brainVol = addRoiToVol(map2vol(anatVol),mask2roi(mask),[1 0 0],2);
-    brainVol_sr = addBinaryMapToVol(brainVol,supraThreshMarked_sr.*mask,[1 1 0]);
-    showVol(brainVol_sr,'signrank, E(FDR) < .05',3)
-    handleCurrentFigure([rootDir,filesep,'DEMO4',filesep,'results_DEMO4_signRank'],userOptions);
-    
-    % display the FDR-thresholded maps on a sample anatomy (t-test) :
-    brainVol = addRoiToVol(map2vol(anatVol),mask2roi(mask),[1 0 0],2);
-    brainVol_t = addBinaryMapToVol(brainVol,supraThreshMarked_t.*mask,[1 1 0]);
-    showVol(brainVol_t,'t-test, E(FDR) < .05',4)
-    handleCurrentFigure([rootDir,filesep,'DEMO4',filesep,'results_DEMO2_tTest'],userOptions);
-end
-cd(rootDir);
 
+%% Run statistical tests at each voxel
+[pMap_ttest, pMap_signrank] = TestAllVoxels(rMaps,SLMetadata);
+
+%% Plot solutions
+load(fullfile(userOptions.rootPath,'Maps','SimulationMetadata.mat'), 'MaskFromSimulation');
+SLMetadata.MaskFromSimulation = MaskFromSimulation;
+
+PlotSearchlightResults( pMap_ttest, 'ttest', SLMetadata, userOptions, 'PlotSimulatedEffect', true, 'SimFigureNumber',2, 'FigureNumber',3);
+PlotSearchlightResults( pMap_signrank, 'signedrank', SLMetadata, userOptions, 'PlotSimulatedEffect', false,'FigureNumber',4 );
